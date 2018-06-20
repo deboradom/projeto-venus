ba304ad1fef3d25cb2ff4487297a09c61552ef5e
deboradom <deboradom@gmail.com>
2018-06-19 22:10:10 -0300

    input search javascript

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+function buscar(){
+    let input, filter, cadastradas, cards, a, i;
+    input = document.getElementById("busca");
+    filter = input.value.toUpperCase();
+    cadastradas = document.getElementsByClassName("cadastradas");
+    cards = document.getElementsByClassName("cards");
+
+    for(i=0; i < cards.length; i++){
+        a = cards[i].getElementsByClassName("descricao")[0];
+        if(a.innerHTML.toUpperCase().indexOf(filter) > -1){
+            cards[i].style.display = "";
+        }else{
+            cards[i].style.display = "none";
+        }    
+    }
+}
+
+
+// function busca(){
+//     let input, filter, ul, li, a, i;
+//     input = document.getElementById("busca");
+
+
+
+
+// }
